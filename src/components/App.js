e3155213c9acffd05e47a0d5bde86b7f2c2669ce
= <shivampathak19972@gmail.com>
2026-01-06 14:41:03 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import Calculator from "./Calculator";
+import "../styles/App.css";
 
+function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
